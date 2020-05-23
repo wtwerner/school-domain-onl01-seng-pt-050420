5cd8efa95423414baace78ccc3800d22ad9e9625
Tommy Werner <wtwerner@gmail.com>
2020-05-23 00:11:59 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,8 +1,13 @@
 class School
+  attr_accessor :student, :grade
   def initialize(name)
     @name=name
   end
   def name
     @name
+  end
+  roster = {}
+  def add_student(student, grade)
+    roster[grade] << student
   end
 end
