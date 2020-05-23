1db2e5d664c7e64b78927963a272e17b3295fbac
Tommy Werner <wtwerner@gmail.com>
2020-05-23 00:34:54 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -23,8 +23,8 @@ class School
     @roster[grade]
   end
   def sort
-    @roster.each do |grade, students|
-      @roster[grade] << @roster[grade][students].to_s.sort
+    @roster.each do |grade, student|
+      @roster[grade] << @roster[grade].sort
     end
   end
 end
